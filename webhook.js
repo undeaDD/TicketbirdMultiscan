72e66b42231c0d6ec1d6fbdae4fafd09b2bb03eb
Dominic Drees <dominic.drees@live.de>
2022-01-22 19:00:56 +0100

    webhook template added

--- webhook.js
+++ webhook.js
@@ -1,11 +1,14 @@
 module.exports = () => {
+	const tgToken = "1801707990:AAFp5mXmyWychgL1shcAX1s2O0xuGbs3iOA";
+	const chatId = "20932747";
+	const message = "%5BExpo%5D%20new%20version%20published";
+
 	require("https")
-		.request(
-			{
-				hostname: "api.telegram.org",
-				path: "/bot<TOKEN>/sendMessage?",
-			},
-			res => {},
-		)
-		.end();
+	.request({
+		hostname: "api.telegram.org",
+		port: 443,
+		path: "/bot" + tgToken + "/sendMessage?chat_id=" + chatId + "&text=" + message + "&parse_mode=markdown",
+		method: "GET",
+	}, _res => {})
+	.end();
 };
